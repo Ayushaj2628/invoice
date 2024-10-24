447f206d7b11dbc1b64902558c3aec48459965cb
Ayush Jain <28.jainayush@gmail.com>
2024-10-24 15:43:56 +0530

    feat: update table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -11,7 +11,7 @@ import { Component, Input } from '@angular/core';
 export class TableComponent {
     @Input() headers: string[] = [];
   @Input() datas = <any>[];
- @Input() displayColumns = [];
+ @Input() displayColumns = <any> [];
 
 
 
